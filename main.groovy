088a9a86df0d957e6d4b7ea5bdc7a2c2fcb0aaf6
Дмитрий Озолин Валерьевич <d.ozolin@mfcrb.ru>
2022-02-23 15:19:19 +0500

    add pipline script

---
+++ main.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any 
+    
+    stages { 
+        stage('Clone repo and chenge config') { 
+            steps { 
+                git branch: "master", url: "https://github.com/Cmertho/sber_ansible.git"
+                writeFile file: 'inventory/hosts', text: "$HOSTS_ITEMS"
+                writeFile file: 'inventory/host_vars/db', text: "greenplum_admin_user: $GREENPLUM_USER\ngreenplum_admin_password: $GREENPLUM_PASSWORD\n"
+            }
+        }
+     stage('Deploy db'){
+        steps{
+            ansiblePlaybook inventory: 'inventory/', playbook: 'deploy-db-nginx.yml', tags: 'install_db'
+        }    
+    }
+    stage('Test install nginx with running db'){
+        steps{
+            ansiblePlaybook inventory: 'inventory/', playbook: 'deploy-db-nginx.yml', tags: 'test_install_nginx'
+        }  
+    }
+    stage('Test install nginx with not running db'){
+        steps{
+            ansiblePlaybook inventory: 'inventory/', playbook: 'deploy-db-nginx.yml', tags: 'test_not_install_nginx'
+        }  
+    }
+    }
+}
